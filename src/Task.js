a385e072ccf5caadd6de6a1debf12144ab2f5957
yusronnugroho <yusronx2smaboy@gmail.com>
2023-06-25 09:46:52 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "";
+const studentName = "Yusron Nugroho Aji";
+const studentId = "FE4666470";
+
+export { url, studentName, studentId }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import { Box, Text, Flex } from "@chakra-ui/react";
+const Footer = () => {
+  return (
+    <Box className="footer">
+      <Flex
+        align="center"
+        justify="center"
+        h="50px"
+        bg="gray.200"
+        borderTop="1px solid"
+        borderColor="gray.300"
+      >
+        <Text className="studentName" mr={2}>
+          Yusron Nugroho Aji
+        </Text>
+        <Text className="studentId">FE4666470</Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Footer;
+
+// const studentName = "Yusron Nugroho Aji";
+// const studentId = "FE4666470";
